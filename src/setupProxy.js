68c9316c631577ac3b0e41ba631c917f1eb82410
ggarabedian <garabedian2001@yahoo.com>
2021-03-15 10:23:34 +0200

    Simplified the proxySetup file

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,31 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
   app.use(
-    "/token",
-    createProxyMiddleware({
-      target: "https://mobile-1.moveitcloud.com/api/v1",
-      changeOrigin: true,
-    })
-  );
-
-  app.use(
-    "/users/self",
-    createProxyMiddleware({
-      target: "https://mobile-1.moveitcloud.com/api/v1",
-      changeOrigin: true,
-    })
-  );
-
-  app.use(
-    "/folders/:directoryId/files",
-    createProxyMiddleware({
-      target: "https://mobile-1.moveitcloud.com/api/v1",
-      changeOrigin: true,
-    })
-  );
-
-  app.use(
-    "/token/revoke",
+    ["/token", "/users/self", "/folders/:directoryId/files", "/token/revoke"],
     createProxyMiddleware({
       target: "https://mobile-1.moveitcloud.com/api/v1",
       changeOrigin: true,
